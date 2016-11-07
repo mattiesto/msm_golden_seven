720fedf22353fdcfaf6fc95b6499a2a05fbf1dc7
Matthias Mehrpuyan <mmehrpuyan@gmail.com>
2016-11-07 16:09:28 -0600

    Final

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,7 +6,39 @@ class DirectorsController < ApplicationController
   def show
     @director = Director.find(params[:id])
   end
-  
+
+  def new_form
+  end
+
+  def create_row
+    @director = Director.new
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
+
+    @director.save
+
+    render("show")
+  end
+
+  def edit_form
+    @director = Director.find(params[:id])
+  end
+
+  def update_row
+    @director = Director.find(params[:id])
+
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
+
+    @director.save
+    
+    redirect_to("/directors/"+@director.id.to_s)
+  end
+
   def destroy
     @director = Director.find(params[:id])
 
